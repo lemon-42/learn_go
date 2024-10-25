0e3c3093b9213040e1bcf85eb4aa32fd7ec16821
lemon_42 <etclegg96@gmail.com>
2024-10-25 11:08:16 +0200

    Add function and code from the tutorial

--- array_slices/sum.go
+++ array_slices/sum.go
@@ -7,3 +7,24 @@ func Sum(numbers []int) int {
 	}
 	return res
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sum []int
+	for _, numbers := range numbersToSum {
+		sum = append(sum, Sum((numbers)))
+	}
+	return sum
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return sums
+}
